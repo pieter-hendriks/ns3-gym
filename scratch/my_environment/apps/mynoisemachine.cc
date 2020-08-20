2765c58fca753b728f5a92f765bfbc82cac18c81
Pieter Hendriks <hendriks.p.pieter@gmail.com>
2020-08-20 12:42:29 +0200

    Fixed noise address

--- scratch/my_environment/apps/mynoisemachine.cc
+++ scratch/my_environment/apps/mynoisemachine.cc
@@ -3,7 +3,7 @@
 #include "ns3/packet.h"
 #include "ns3/net-device.h"
 #include "ns3/ipv4.h"
-
+#include "ns3/mac48-address.h"
 #include <cstdlib>
 
 using namespace ns3;
@@ -16,7 +16,7 @@ void MyNoiseMachine::Send()
 {
 	const auto pkt_size = 1500u;
 	auto pkt = ns3::Create<ns3::Packet>(pkt_size);
-	this->GetNode()->GetDevice(1)->Send(pkt, Ipv4Address("192.168.1.100"), 0);
+	this->GetNode()->GetDevice(1)->Send(pkt, Mac48Address::GetBroadcast(), 0);
 	Simulator::Schedule(ns3::Time::FromInteger(1 + std::rand() % 150, ns3::Time::Unit::MS), &MyNoiseMachine::Send, this);
 }
 void MyNoiseMachine::StopApplication()
