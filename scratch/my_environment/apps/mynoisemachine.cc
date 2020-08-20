5bda293df2d3ed1a521a667b10685afff8ea7903
Pieter Hendriks <hendriks.p.pieter@gmail.com>
2020-08-20 12:55:21 +0200

    NOise fix

--- scratch/my_environment/apps/mynoisemachine.cc
+++ scratch/my_environment/apps/mynoisemachine.cc
@@ -4,7 +4,9 @@
 #include "ns3/net-device.h"
 #include "ns3/ipv4.h"
 #include "ns3/mac48-address.h"
+#include "ns3/wifi-net-device.h"
 #include <cstdlib>
+#include <stdexcept>
 
 using namespace ns3;
 
@@ -16,7 +18,20 @@ void MyNoiseMachine::Send()
 {
 	const auto pkt_size = 1500u;
 	auto pkt = ns3::Create<ns3::Packet>(pkt_size);
-	this->GetNode()->GetDevice(1)->Send(pkt, Mac48Address::GetBroadcast(), 0);
+	ns3::Ptr<ns3::NetDevice> dev = nullptr;
+	for (unsigned i = 0; i < this->GetNode()->GetNDevices(); ++i)
+	{
+		dev = this->GetNode()->GetDevice(i);
+		if (dev->GetTypeId() == WifiNetDevice::GetTypeId())
+		{
+			break;
+		}
+	}
+	if (dev->GetTypeId() != WifiNetDevice::GetTypeId())
+	{
+		throw std::runtime_error("This should be impossible.");
+	}
+	dev->Send(pkt, Mac48Address::GetBroadcast(), 0);
 	Simulator::Schedule(ns3::Time::FromInteger(1 + std::rand() % 150, ns3::Time::Unit::MS), &MyNoiseMachine::Send, this);
 }
 void MyNoiseMachine::StopApplication()
